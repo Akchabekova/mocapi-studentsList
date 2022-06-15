299fb05ac71dbd6cb52797c4fcb4cfae535842ee
akchabekova <Luelum7@gmail.com>
2022-06-15 18:27:21 +0200

    problema s edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,98 @@
+import React, {useEffect, useState} from "react";
 import './App.css';
+import axios from "axios";
+import AddUserModal from "./components/AddUserModal";
+// import 'boxicons';
+
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+    const [students, setStudents] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [openModal, setOpenModal] = useState(false)
+    const [editingUser, setEditingUser] = useState(null)
+
+
+    const deleteUser = async (id) => {
+        await axios.delete(`https://6298e48abf77b60258233216.mockapi.io/students/${id}`)
+        const studentsList = students.filter(item => item.id !== id)
+        setStudents(studentsList)
+  }
+
+    const handleEdit = (student) => {
+        setEditingUser(student)
+        setOpenModal(true)
+    }
+    useEffect(() =>{
+        axios('https://6298e48abf77b60258233216.mockapi.io/students')
+            .then((res) =>{
+                setStudents(res.data)
+                setIsLoading(false)
+            })
+    },[])
+
+    if(isLoading){
+        return "Loading ..."
+    }
+    return (
+        <div className="App">
+            {
+                openModal &&
+                <AddUserModal
+                    setOpenModal = {setOpenModal}
+                    students={students}
+                    setStudents={setStudents}
+                    editingUser={editingUser}
+                    setEditingUser={setEditingUser()}
+                />
+            }
+            <button onClick={() => setOpenModal(true)}
+                    className="inline-flex  pr-5 mt-5 rounded-xl border green py-1 px-4 text-primary bg-green-700  text-white my-6">
+                Добавить студента
+                <box-icon name='user-plus' classname="pl-3"></box-icon>
+            </button>
+
+            <table className="table-auto w-full">
+                <thead>
+                <tr className="bg-blue-500 text-center">
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">#</th>
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">ФИО</th>
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">Группа</th>
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">Год поступления</th>
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">Телефон</th>
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">Почта</th>
+                    <th className="w-1/7 min-w-[160px] text-lg font-semibold text-white py-4 lg:py-7 px-3  lg:px-4 border-l border-transparent">Статус</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    students.map((student) => (
+                        <tr key={student.id}>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">{student.id}</td>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">{student.name}</td>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">{student.group}</td>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">{student.year}</td>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">{student.phone}</td>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">{student.email}</td>
+                            <td className="text-center text-dark font-medium text-base py-5 px-2 bg-[#F3F6FF] border-b border-l border-[#E8E8E8] ">
+                                <button
+                                    onClick={() => handleEdit(student)}
+                                    className=" border border-yellow-400 py-1 px-4 mr-2 text-primary inline-block rounded ">
+                                    Edit
+                                </button>
+                                <button
+                                    onClick={() => deleteUser(student.id)}
+                                    className=" border border-red-400 py-1 px-4 text-primary inline-block rounded ">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+
+                }
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
--- src/components/AddUserModal/AddUserModal.js
+++ src/components/AddUserModal/AddUserModal.js
@@ -1,10 +1,133 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
+import { useFormik } from "formik";
 
-const AddUserModal = () => {
+
+
+const AddUserModal = ({ students, setStudents, setOpenModal, editingUser, setEditingUser}) => {
+  const formik = useFormik({
+        initialValues: {
+            name: "",
+            group: "",
+            year: "",
+            phone: "",
+            email: ""
+        },
+
+        onSubmit: async (values) => {
+            const uploadUser = await axios.post('https://6298e48abf77b60258233216.mockapi.io/students', values)
+            setStudents([...students, uploadUser.data])
+            setOpenModal(false)
+
+        }
+    })
+
     return (
         <div>
-            AddUserModal
-        </div>
+            <div className="App">
+                <div className='fixed justify-center flex w-full bg-white p-6 '>
+                    <div className='absolute right-9 top-9 cursor-pointer text-black-500' onClick={() => {
+                        setOpenModal(false)
+                        setEditingUser(null)
+                    }}>
+                        {<box-icon name='x'></box-icon>}
+                        x
+                    </div>
+                    <form onSubmit={ formik.handleSubmit }>
+                        <div>
+                            <div className="bg-white shadow rounded-lg p-6">
+                                <div className="grid lg:grid-cols-2 gap-6">
+                                    <div
+                                        className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+                                        <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+                                            <p>
+                                                <label htmlFor="name" className="bg-white text-gray-600 px-1">ФИО*</label>
+                                            </p>
+                                        </div>
+                                        <p>
+                                            <input onChange={formik.handleSubmit}
+                                                   id="name"
+                                                   name="name" autoComplete="false" tabIndex="0"
+                                                   type="text"
+                                                   className="py-1 px-1 text-gray-900 outline-none block h-full w-full"
+                                                   value={formik.values.name}
+                                            />
+                                        </p>
+                                    </div>
+                                    <div
+                                        className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+                                        <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+                                            <p>
+                                                <label htmlFor="group" className="bg-white text-gray-600 px-1">Группа*</label>
+                                            </p>
+                                        </div>
+                                        <p>
+                                            <input onChange={formik.handleSubmit} name="group" autoComplete="false" tabIndex="0"
+                                                   type="text"
+                                                   id="group"
+                                                   className="py-1 px-1 outline-none block h-full w-full"
+                                                   value={formik.values.group}/>
+                                        </p>
+                                    </div>
+                                    <div
+                                        className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+                                        <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+                                            <p>
+                                                <label htmlFor="year" className="bg-white text-gray-600 px-1">Год*</label>
+                                            </p>
+                                        </div>
+                                        <p>
+                                            <input onChange={formik.handleSubmit} name="year" autoComplete="false" tabIndex="0"
+                                                   type="date"
+                                                   id="year"
+                                                   className="py-1 px-1 outline-none block h-full w-full"
+                                                   value={formik.year}/>
+                                        </p>
+                                    </div>
+                                    <div
+                                        className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+                                        <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+                                            <p>
+                                                <label htmlFor="phone" className="bg-white text-gray-600 px-1">Телефон*</label>
+                                            </p>
+                                        </div>
+                                        <p>
+                                            <input onChange={formik.handleSubmit} name="phone" autoComplete="false" tabIndex="0"
+                                                   type="text"
+                                                   id="phone"
+                                                   className="py-1 px-1 outline-none block h-full w-full text-black"
+                                                   value={formik.values.phone}/>
+                                        </p>
+                                    </div>
+                                    <div
+                                        className="border focus-within:border-blue-500 focus-within:text-blue-500 transition-all duration-500 relative rounded p-1">
+                                        <div className="-mt-4 absolute tracking-wider px-1 uppercase text-xs">
+                                            <p>
+                                                <label htmlFor="email" className="bg-white text-gray-600 px-1">Почта*</label>
+                                            </p>
+                                        </div>
+                                        <p>
+                                            <input onChange={formik.handleSubmit} name="email" autoComplete="false" tabIndex="0"
+                                                   type="text"
+                                                   id="email"
+                                                   className="py-1 px-1 outline-none block h-full w-full"
+                                                   value={formik.values.email}/>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="border-t mt-6 pt-3">
+                                    <button
+                                        className="rounded text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300">
+                                        {editingUser ? "Save" : "Create"}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            )
+     </div>
     );
 };
 
